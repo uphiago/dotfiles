52128e827729f757ffc61b7d0d37e0734edbf248
uphiago <hollyvolks@hotmail.com>
2024-09-10 04:59:20 -0300

    Auto-commit: Config files updated. 2024-09-10 04:59:20

--- commit.sh
+++ commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
 git config --global --add safe.directory /home/volks/Repositories/personal/configs/dotfiles
 
 REPO_PATH="/home/volks/Repositories/personal/configs/dotfiles"
