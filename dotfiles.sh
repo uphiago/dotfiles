97df7aa4a74fad5c9c2b7a726ac53f0408d84427
uphiago <hollyvolks@hotmail.com>
2025-03-17 18:30:19 -0300

    Auto-update 2025-03-17_18:30:19

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
 
 REPO="$HOME/Repositories/devops/dotfiles"
-ZSH="$REPO/.zshrc"
-ALA="$REPO/alacritty.toml"
-FFC="$REPO/config.jsonc"
 
-git config --global --add safe.directory "$REPO"
+[ ! -d "$REPO/.git" ] && echo "Not a valid Git repo: $REPO" && exit 1
 
-[ ! -d "$REPO" ] && echo "Repo not found: $REPO" && exit 1
+cd "$REPO" || exit 1
 
-cd "$REPO"
-git pull origin main
-
-ln -sf "$ZSH" "$HOME/.zshrc"
-mkdir -p "$HOME/.config/alacritty"
-ln -sf "$ALA" "$HOME/.config/alacritty/alacritty.yml"
-mkdir -p "$HOME/.config/fastfetch"
-ln -sf "$FFC" "$HOME/.config/fastfetch/config.jsonc"
+git pull --rebase origin main
 
 git add .
-git commit -m "Update dotfiles $(date +%F_%T)"
+git commit -m "Auto-update $(date +%F_%T)" || true
+
 git push origin main
